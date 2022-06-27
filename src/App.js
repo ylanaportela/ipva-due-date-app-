88e0a3bc820e3a4b894d9bec4172c766c73840bd
Ylana Portela <ylanaportela2000@gmail.com>
2022-06-27 17:39:48 -0300

    change file app with feature of validation of the plate and get the end of plate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [input, setInput ] = useState('')
+
+  const regex = RegExp('[A-Z]{3}[0-9][0-9A-Z][0-9]{2}')
+
+  function handleSubmit(){
+    console.log(regex.test(input))
+    console.log(input.slice(-1))
+  }
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div>
+
+        <label>
+          Digite a placa do seu carro
+          <input value={input} onChange={e => setInput(e.target.value.toLocaleUpperCase())}/>
+        </label>
+        <div>
+        Selecione o estado que o seu carro foi emplacado
+        <select>
+          <option value="bahia">BA</option>
+          <option value="ceara" selected>CE</option>
+        </select>
+        </div>
+
+        <button type="submit" onClick={handleSubmit}>Verificar</button>
+      </div>
     </div>
   );
 }
-
 export default App;
