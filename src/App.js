0eb9c75fec9af3d321e134405a89a8e34d794ee2
Ylana Portela <ylanaportela2000@gmail.com>
2022-07-11 18:04:57 -0300

    Add feature of get the end the plate and render the datas of for each plate

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+import data from './tableBahia.json'
 
 function App() {
-  const [input, setInput ] = useState('')
+  const [input, setInput] = useState('')
+  const [endPlate, setEndPlate] = useState()
 
   const regex = RegExp('[A-Z]{3}[0-9][0-9A-Z][0-9]{2}')
 
-  function handleSubmit(){
-    console.log(regex.test(input))
-    console.log(input.slice(-1))
+  function handleSubmit() {
+    if (regex.test(input)) {
+      setEndPlate(input.slice(-1))
+    }
   }
 
-  
+  const fidnEndPlate = useMemo(() => {
+
+    const foundId = data.table.find((item) => {
+      return item.identification === Number(endPlate)
+    })
+    if(foundId !== undefined){
+      return (
+        <>
+          <div>{foundId.identification}</div>
+          <div>{foundId.expiration}</div>
+        </>
+      ) 
+    }
+
+  }, [endPlate])
+
+
 
   return (
     <div className="container">
@@ -18,17 +37,22 @@ function App() {
 
         <label>
           Digite a placa do seu carro
-          <input value={input} onChange={e => setInput(e.target.value.toLocaleUpperCase())}/>
+          <input value={input} onChange={e => setInput(e.target.value.toLocaleUpperCase())} type="text" maxLength={7} />
         </label>
         <div>
-        Selecione o estado que o seu carro foi emplacado
-        <select>
-          <option value="bahia">BA</option>
-          <option value="ceara" selected>CE</option>
-        </select>
+          Selecione o estado que o seu carro foi emplacado
+          <select>
+            <option value="bahia">BA</option>
+            <option value="ceara" selected>CE</option>
+          </select>
         </div>
 
         <button type="submit" onClick={handleSubmit}>Verificar</button>
+        <div>
+          
+        {fidnEndPlate}
+          
+        </div>
       </div>
     </div>
   );
